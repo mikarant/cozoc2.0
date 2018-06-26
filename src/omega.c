75b5ef767ae2467cf20cd898cd40fa349545a69b
Rantanen Mika P <mika.p.rantanen@helsinki.fi>
2018-06-26 09:19:44 +0300

    temperature advection forcing variable removed

--- src/omega.c
+++ src/omega.c
@@ -188,6 +188,7 @@ extern PetscErrorCode omega_compute_rhs_F_T (
     Vec         T   = ctx->Temperature;
     Vec         V   = ctx->Horizontal_wind;
     Vec          s    = ctx->Surface_attennuation;
+    Vec          tadv = ctx->Temperature_advection;
     PetscScalar hx  = ctx->hx;
     PetscScalar hy  = ctx->hy;
     PetscScalar hz  = ctx->hz;
@@ -196,9 +197,11 @@ extern PetscErrorCode omega_compute_rhs_F_T (
     PetscInt       i, j, k, zs, ys, xs, zm, ym, xm;
     const double r = earth_radius;
 
-    VecCopy (T, b);
-    horizontal_advection (b, V, ctx);
+//    VecCopy (T, b);
+//    horizontal_advection (b, V, ctx);
 
+    VecCopy (tadv, b);
+    VecScale(b,-1.0);
     VecPointwiseMult(b, s, b);
     plaplace (b, ctx);
 
--- src/targets.c
+++ src/targets.c
@@ -43,7 +43,7 @@ Targets new_targets (Options options, Files files, Context *ctx) {
                             .vec   = ctx->Vorticity_advection},
                     .time = options.first - 1},
 
-                [TARGET_FIELD_TEMPERATURE_ADVECTION_FORCING] =
+                [TARGET_FIELD_TEMPERATURE_ADVECTION] =
                 (Target){
                     .type = TARGET_TYPE_FIELD,
                     .field =
