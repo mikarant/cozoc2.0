257755d3814168ef5fc3b4c63466fa2a2148c248
Rantanen Mika P <mika.p.rantanen@helsinki.fi>
2018-08-16 11:06:11 +0300

    better value for sigmamin

--- src/constants.c
+++ src/constants.c
@@ -3,6 +3,6 @@
 const double Gravitational_acceleration       = 9.80665;    // (m/s^2)
 const double Specific_gas_constant_of_dry_air = 287.058;    // R
 const double Specific_heat_of_dry_air         = 1004.0;     // c_p
-const double sigmamin                         = 2.0e-7;
+const double sigmamin                         = 1.0e-6;
 const double etamin                           = 2.0e-6;
 const double earth_radius                     = 6370000.0;
